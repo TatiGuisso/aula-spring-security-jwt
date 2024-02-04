31b30c5e0081ed8e0f101f84fc033eba17e1a792
tatiane <tati.guisso@gmail.com>
2024-02-04 11:27:34 -0300

    configurações iniciais

--- src/main/java/com/springsecutiry/fiap/SpringSecurityJWT.java
+++ src/main/java/com/springsecutiry/fiap/SpringSecurityJWT.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FiapApplication {
+public class SpringSecurityJWT {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FiapApplication.class, args);
+		SpringApplication.run(SpringSecurityJWT.class, args);
 	}
 
 }
